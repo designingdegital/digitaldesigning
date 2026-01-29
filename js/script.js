a38368746de7fc423ef255324a3b57f097ad4ef5
designingdegital <designingdegital@gmail.com>
2026-01-29 21:00:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-function scrollToSection(id) {
-  const el = document.getElementById(id);
-  if (!el) return;
+// function scrollToSection(id) {
+//   const el = document.getElementById(id);
+//   if (!el) return;
 
-  el.scrollIntoView({
-    behavior: "smooth",
-    block: "start"
-  });
-}
+//   el.scrollIntoView({
+//     behavior: "smooth",
+//     block: "start"
+//   });
+// }
 // Smooth scroll
 function scrollToServices() {
   document.getElementById("services").scrollIntoView({ behavior: "smooth" });
